316b427ee65882558e5857e40a23cf2f6a05b24c
Nathanael <nathanael.pivot@ynov.com>
2023-10-12 09:33:55 +0200

    test

---
+++ jeu/hangman.go
@@ -0,0 +1,8 @@
+package hangman
+
+var a = "dindon"
+
+func Check(a string) string {
+	a = "patate"
+	return a
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	h "hangman/jeu"
+)
+
+var a = "dindon"
+
+func main() {
+	fmt.Println(h.Check(a))
+}
