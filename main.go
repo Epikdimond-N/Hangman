d0889d053289a3ad0446ac1ace7cf536ff14655a
PG_alexandre <alexandre.petitghanem@ynov.com>
2023-10-13 12:13:28 +0200

    maxi commit

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
 	h "hangman/jeu"
 )
 
 func main() {
 	h.Recup()
+	h.Convert()
+	h.Convertstr()
+	h.Motrandom()
+	fmt.Println(h.MotR)
+	//fmt.Println(h.TabData[0])
+
 }
